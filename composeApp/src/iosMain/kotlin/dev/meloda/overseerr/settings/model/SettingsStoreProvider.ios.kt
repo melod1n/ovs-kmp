96e942ccc0c616bac8e40de4209035314f8e2a21
Danil Nikolaev <lischenkodev@gmail.com>
2024-08-05 06:59:12 +0300

    fix ios settings store provider

--- composeApp/src/iosMain/kotlin/dev/meloda/overseerr/settings/model/SettingsStoreProvider.ios.kt
+++ composeApp/src/iosMain/kotlin/dev/meloda/overseerr/settings/model/SettingsStoreProvider.ios.kt
@@ -2,6 +2,7 @@ package dev.meloda.overseerr.settings.model
 
 import dev.meloda.overseerr.appDir
 import io.github.xxfast.kstore.KStore
+import io.github.xxfast.kstore.file.storeOf
 import okio.Path.Companion.toPath
 
 actual class SettingsStoreProvider actual constructor() {
