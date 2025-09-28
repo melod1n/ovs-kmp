715c4ba40bf76ae4627ab7e32189127d3ad563fe
Danil Nikolaev <lischenkodev@gmail.com>
2025-09-28 23:01:07 +0300

    revert to stable material3 version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -78,8 +78,7 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-//            implementation(compose.material3)
-            implementation(libs.material3)
+            implementation(compose.material3)
             implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
             implementation(libs.androidx.navigation.compose)
