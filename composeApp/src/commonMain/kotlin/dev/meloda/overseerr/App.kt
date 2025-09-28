402e71cb60a0ef7812e75fd7439be2527764ed39
Danil Nikolaev <lischenkodev@gmail.com>
2025-09-28 21:31:31 +0300

    set web app size to mobile

--- composeApp/src/commonMain/kotlin/dev/meloda/overseerr/App.kt
+++ composeApp/src/commonMain/kotlin/dev/meloda/overseerr/App.kt
@@ -1,14 +1,19 @@
 package dev.meloda.overseerr
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import dev.meloda.overseerr.screens.main.MainScreen
 import dev.meloda.overseerr.datastore.SettingsController
+import dev.meloda.overseerr.screens.main.MainScreen
 import dev.meloda.overseerr.theme.AppTheme
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
@@ -29,9 +34,18 @@ internal fun App() {
         settingsController.loadAppSettings()
     }
 
-    AppTheme(themeMode = settings.themeMode) {
-        Surface(modifier = Modifier.fillMaxSize()) {
-            MainScreen()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AppTheme(themeMode = settings.themeMode) {
+            Surface(
+                modifier = Modifier
+                    .width(360.dp)
+                    .height(640.dp)
+            ) {
+                MainScreen()
+            }
         }
     }
 }
